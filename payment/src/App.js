9d03ace1ba30c0410b3ae005be96739aad282dc8
sun <hama006@flroad.co.kr>
2022-02-04 15:21:10 +0900

    serverSideTest

---
+++ payment/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <form>
+        <input type="text"></input>  
+      </form>
+    
+    </div>
+  );
+}
+
+export default App;
--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require("express"); // express 선언
+const path = require("path");
 const app = express();  //  express 객체 담기
 
 const router = require("./route/route");    //  router 가져오기
 
 app.use(express.json());    // req.body로 데이터 받아오기
 app.use(express.urlencoded({extended:false}));  // req.body로 데이터 받아오기
+
+app.use(express.static(path.join(__dirname, 'payment/build')));// react build 파일 static 파일로 사용
+
+app.get("/react",(req,res)=>{
+    res.sendFile(path.join(__dirname, '/payment/build/index.html'));//   /react로 접속시 보여줄 파일 적용
+})
 
 app.use(router);    // 라우터로 분기 시키기
 
